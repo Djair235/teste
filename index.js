e96199ff4abf5db1ad482317bf55573b66df2843
Djair235 <140333747+Djair235@users.noreply.github.com>
2023-07-23 15:37:41 -0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function esconder() {
+    var botão_de_esconder = window.document.getElementById('foto')
+    botão_de_esconder.style.visibility = "hidden"
+}
+
+function mostrar() {
+    var botão_de_mostrar = window.document.getElementById('foto')
+    botão_de_mostrar.style.visibility = "visible"
+}
